d198134cd4db192061a8029dfb8a25f453090d7d
emily c. ritter <emily.c.ritter@gmail.com>
2021-03-30 13:17:42 -0500

    Creates basic actions and reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { selectSong } from '../actions';
+import { songsReducer, selectedSongReducer } from '../reducers';
 
 const App = () => {
   return (
     <div>
-    App
+      App
     </div>
   );
 }
